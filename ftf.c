f268a08f14edcc03956d8bb48463ac6dbe6f77d3
tmarts <tmarts@student.42heilbronn.de>
2023-01-06 23:05:56 +0100

    line drawing works, axis wrong

--- ftf.c
+++ ftf.c
@@ -6,7 +6,7 @@
 /*   By: tmarts <tmarts@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 18:35:08 by tmarts            #+#    #+#             */
-/*   Updated: 2023/01/05 20:02:00 by tmarts           ###   ########.fr       */
+/*   Updated: 2023/01/06 22:57:29 by tmarts           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include "MLX42/include/MLX42/MLX42.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 //converting 3d cordinares to 2d flash cordinates
 int	x_fla(int x, int y, int z, int x_origin)
@@ -36,41 +37,108 @@ int	y_fla(int x, int y, int z, int y_origin)
 	return (y_isom);
 }
 
-style = function (a, b, c) {
-// a: line width
-// b: line color
-// c: line alpha
-lineStyle(a, b, c);
-};
-
-plot = function (x, y, z) {
-moveTo(xFla(x, y, z), yFla(x, y, z));
-};
-
-draw = function (x, y, z) {
-lineTo(xFla(x, y, z), yFla(x, y, z));
-};
-
-void	draw_line(mlx_image_t *img, int x1, int y1, int x2, int y2)
+void	draw_line_low(mlx_image_t *img, int x1, int y1, int x2, int y2)
 {
 	int	dx;
 	int	dy;
 	int	p;
+	int	y_step;
 
+	y_step = 1;
 	dx = x2 - x1;
 	dy = y2 - y1;
+	if (dy < 0)
+	{
+		y_step = -1;
+		dy = -dy;
+	}	
 	p = 2 * dy - dx;
 	while (x1 < x2)
 	{
-		mlx_put_pixel(img, x1, y1, 0xEE4B2BFF);
-		x1++;
+		mlx_put_pixel(img, x1++, y1, 0xEE4B2BFF);
 		if (p < 0)
 			p = p + 2 * dy;
 		else
 		{
 			p = p + 2 * dy - 2 * dx;
-			y1++;
+			y1 = y1 + y_step;
 		}
-	}	
+	}
 }
 
+void	draw_line_high(mlx_image_t *img, int x1, int y1, int x2, int y2)
+{
+	int	dx;
+	int	dy;
+	int	p;
+	int	x_step;
+
+	x_step = 1;
+	dx = x2 - x1;
+	dy = y2 - y1;
+	if (dx < 0)
+	{
+		x_step = -1;
+		dx = -dx;
+	}	
+	p = 2 * dx - dy;
+	while (y1 < y2)
+	{
+		mlx_put_pixel(img, x1, y1++, 0xEE4B2BFF);
+		if (p < 0)
+			p = p + 2 * dx;
+		else
+		{
+			p = p + 2 * dx - 2 * dy;
+			x1 = x1 + x_step;
+		}
+	}
+}
+
+void	draw_line(mlx_image_t *img, int x1, int y1, int x2, int y2)
+{
+
+	if (abs(y2 - y1) < abs(x2 - x1))
+	{
+		if (x1 > x2)
+			draw_line_low(img, x2, y2, x1, y1);
+		else
+			draw_line_low(img, x1, y1, x2, y2);
+	}
+	else
+	{
+		if (y1 > y2)
+			draw_line_high(img, x2, y2, x1, y1);
+		else
+			draw_line_high(img, x1, y1, x2, y2);
+	}
+}
+
+int	main (void)
+{
+	void		*mlx_ptr;
+	mlx_image_t	*img;	
+	int			x_o = 400/2;
+	int			y_o = 300-30;
+
+	mlx_ptr = mlx_init(400, 300, "mlx 42", true);
+	if (!mlx_ptr)
+		exit(EXIT_FAILURE);
+	img = mlx_new_image(mlx_ptr, 400, 300);
+	if (!img)
+		exit(EXIT_FAILURE);
+	if (mlx_image_to_window(mlx_ptr, img, 0, 0) < 0)
+		exit(EXIT_FAILURE);
+	printf("x_fla %d\n", x_fla(200, 0, 0, x_o));
+
+	printf("y_fla %d\n", y_fla(200, 0, 0, y_o));
+	draw_line(img, x_fla(0, 0, 200, x_o), y_fla(0, 0, 200, y_o), x_o, y_o);
+	draw_line(img, x_fla(0, 200, 0, x_o), y_fla(0, 200, 0, y_o), x_o, y_o);
+	draw_line(img, x_fla(-30, 200, 0, x_o), y_fla(0, 200, 0, y_o), x_o, y_o);
+	draw_line(img, x_fla(30, 200, 0, x_o), y_fla(0, 200, 0, y_o), x_o, y_o);
+	draw_line(img, x_fla(200, 0, 0, x_o), y_fla(200, 0, 0, y_o), x_o, y_o);
+	mlx_loop(mlx_ptr);
+	mlx_delete_image(mlx_ptr, img);
+	mlx_terminate(mlx_ptr);
+	return (EXIT_SUCCESS);
+}
