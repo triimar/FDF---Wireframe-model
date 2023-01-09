cedf36c5308db372ad4125b77275fc55d93cf7d6
tmarts <tmarts@student.42heilbronn.de>
2023-01-09 18:13:21 +0100

    Makefile and header, start

---
+++ fdf.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmarts <tmarts@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/01 16:45:42 by tmarts            #+#    #+#             */
+/*   Updated: 2023/01/09 16:10:59 by tmarts           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <math.h>
+# include "libft/libft.h"
+# include "MLX42/include/MLX42.h"
+# include "MLX42/include/MLX42_Int.h"
+# include "MLX42/MLX42_Input.h"
+
+
+#endif
